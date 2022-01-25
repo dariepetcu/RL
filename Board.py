801b75f459c53ab887f708a6c110779fb6fb88ed
dariepetcu <dariepetcu@gmail.com>
2022-01-25 22:19:51 +0100

    gauss on f computer

---
+++ Board.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Board:
+    def __init__(self, height = 6, width = 7, goal = 4):
+        """
+        Initializes the connect-X board object
+        """
+        self.height = height
+        self.width = width
+        self.goal = goal
+        self.game = np.empty([self.height, self.width])
+
+    def valid_move(self, column):
+        if self.game[self.height][column] is None:
+            return True
+        else:
+            return False
