cde7e5b5100ac3067374b0b95f9086d71ba06196
Farrukh Baratov <farrukhsbaratov@gmail.com>
2022-01-28 23:32:40 +0100

    Changes by darie

--- agent.py
+++ agent.py
@@ -50,15 +50,21 @@ class Agent:
         self.gamma = gamma  # discount factor
         self.epsilon = epsilon  # epsilon for epsilon-greedy
 
-        # length of eligibility trace
-        self.depth = self.game.rows * self.game.columns  #
-        self.decay = 0.8
-
     def reset(self):
+        """
+        Method used to reset information that is not relevant outside
+        the scope of the current episode
+        """
         self.G = []
 
     def brick_layer(self):
+        """
+        Method implementing the brick layering agent, which chooses the column to the right
+        of the one chosen in the previous round
+        :return: the column it chooses for piece placing
+        """
         self.current_sel += 1
+        # prevent selection from going outside board or being invalid
         while self.current_sel not in self.game.valid_moves():
             self.current_sel += 1
             self.current_sel %= self.game.columns
@@ -66,9 +72,17 @@ class Agent:
         return self.current_sel % self.game.columns
 
     def random_agent(self):
+        """
+        Method implementing the random agent, which chooses a random valid move
+        :return: the chosen move
+        """
         return random.choice(self.game.valid_moves())
 
     def select_action(self):
+        """
+        Action selection method called by the trainer to ask agent what its intention is
+        :return: the chosen action
+        """
         match self.selection:
             case Selection.GREEDY:
                 action = self.greedy()
@@ -94,6 +108,11 @@ class Agent:
         return action
 
     def update_estimates(self, reward):
+        """
+        Method called by the trainer to tell agent it can update its estimation
+        based on the newly received information (reward and new state)
+        :param reward: reward for reaching s_{t+1}, is zero unless s_{t+1} is final state
+        """
         if self.selection in [Selection.BRICK, Selection.RANDOM]:
             return
         match self.learning:
@@ -108,20 +127,28 @@ class Agent:
                 return
 
     def Q(self, state, action):
-        return self.Qpairs.get(state, [0] * self.game.columns)[action]
+        """
+        Method used to fetch value estimates from the dict object that stores them
+        :params state, action: pair whose Q-estimate must be fetched
+        :return: fetched Q-value for passed state, action.
+        """
+        return self.Qpairs.get(state, [random.random()] * self.game.columns)[action]
 
     def renew_Q(self, state, action, updated_Q):
-        new_entry = self.Qpairs.get(state, [0] * self.game.columns)
+        """
+        Method to edit value estimates in the dictionary
+        :params state, action: used to target the value estimate to be updated
+        :param updated_Q: the updated value estimate
+        """
+        new_entry = self.Qpairs.get(state, [random.random()] * self.game.columns)
         new_entry[action] = updated_Q
         self.Qpairs[state] = new_entry
 
     def montecarlo(self, reward):
         """
-        Only called after game is finished.
-        :param reward:
-        :return:
+        Only called after game is finished. Computes discounted reward for each state
+        :param reward: used for computing G[state]
         """
-        turns = self.turns
         turns, history = self.game.get_player_history(self.name)
         self.G = [0] * turns
         # start from turns-1 because G[turns] = 0
@@ -130,6 +157,12 @@ class Agent:
         self.mc_update(turns, history)
 
     def mc_update(self, turns, history):
+        """
+        Once discounted reward G[t] has been calculated for current match, this method
+        updates the value estimates based on it
+        :param turns: the total number of moves from current match
+        :param history: list of all state, action pairs from current match
+        """
         for i in range(turns):
             state = history[i][0]
             action = history[i][1]
@@ -137,64 +170,57 @@ class Agent:
             updated_Q += self.alpha * (self.G[i] - updated_Q)
             self.renew_Q(state, action, updated_Q)
 
-
     def QLearn(self, reward):
+        """
+        Method to update value estimate of current state, action when using QLearning
+        :param reward: received for reaching s_{t+1}
+        """
         state, action, next_state, next_action = self.get_sa_pairs()
         updated_Q = self.Q(state, action)
         updated_Q += self.alpha * self.TD_error(state, action, reward, next_state, next_action)
         self.renew_Q(state, action, updated_Q)
 
     def update_SARSA(self, reward):
+        """
+        Method to update value estimate of current state, action when using SARSA
+        :param reward: received for reaching s_{t+1}
+        """
         state, action, next_state, next_action = self.get_sa_pairs()
         updated_Q = self.Q(state, action)
         updated_Q += self.alpha * self.TD_error(state, action, reward, next_state, next_action)
         self.renew_Q(state, action, updated_Q)
 
-    def TD_error(self, state, action, reward, next_state, next_action, lookback=1):
+    def TD_error(self, state, action, reward, next_state, next_action):
+        """
+        Method to compute the TD-Learning according to the selected TD algorithm (on/off policy)
+        :param state: current state
+        :param action: action to go from s to s_{t+1}
+        :param reward: reward for getting to s_{t+1}
+        :param next_state: predicted state s_{t+1}
+        :param next_action: predicted action a_{t+1}
+        :return: TD-error value based on value estimates and hyperparameters
+        """
+        if self.game.get_winner is not None:
+            # Q value of final state is 0, so TD-target = reward
+            return reward - self.Q(state, action)
         match self.learning:
             case Learning.Q:
-                return (reward + self.gamma * max(self.Qpairs.get(next_state, [0] * self.game.columns)) -
+                return (reward + self.gamma * max(self.Qpairs.get(next_state, [random.random()] * self.game.columns)) -
                                    self.Q(state, action))
             case Learning.SARSA:
                 return reward + self.gamma * self.Q(next_state, next_action) - self.Q(state, action)
 
-
-    def eligibility_trace(self, reward, lookback):
-        for idx in range(self.depth):
-            if self.learning == Learning.Q:
-                pass
-            elif self.learning == Learning.SARSA:
-                pass
-
-
-    # lamb = 0.95  # the lambda weighting factor
-    # state = env.reset()  # start the environment, get the initial state
-    # # Run the algorithm for some episodes
-    # for t in range(n_steps):
-    #     # act according to policy
-    #     action = policy(state)
-    #     new_state, reward, done = env.step(action)
-    #     # Update eligibilities
-    #     eligibility *= lamb * gamma
-    #     eligibility[state] += 1.0
-    #
-    #     # get the td-error and update every state's value estimate
-    #     # according to their eligibilities.
-    #     td_error = reward + gamma * state_values[new_state] - state_values[state]
-    #     state_values = state_values + alpha * td_error * eligibility
-    #
-    #     if done:
-    #         state = env.reset()
-    #     else:
-    #         state = new_state
-
-
     def greedy(self):
+        """
+        Action selection method. Chooses valid action with best value estimation
+        :return: chosen action
+        """
         state = self.game.get_state(mark=self.name)
         best_actions = []
         valid_moves = self.game.valid_moves()
         # initialize best move as random valid one
-        best_estimate = self.Qpairs.get(state, [0] * self.game.columns)[random.choice(valid_moves)]
+        best_actions.append(random.choice(valid_moves))
+        best_estimate = self.Qpairs.get(state, [random.random()] * self.game.columns)[best_actions[0]]
 
         for action in valid_moves:
             estimate = self.Q(state, action)
@@ -223,8 +249,7 @@ class Agent:
 
     def get_sa_pairs(self):
         """
-        returns S,A,S,A sequence
-        :return: s_t, a_t, s_{t+1}, a_{t+1}
+        :return: s_t, a_t, s_{t+1}, a_{t+1} sequence
         """
         turns, history = self.game.get_player_history(self.name, n=2)
         state, action = history[0]
@@ -235,8 +260,8 @@ class Agent:
 
     def set_hyperparameter(self, hyper, value):
         """
-        Setter. Sets the value of a provided hypeparameter. Used for tuning.
-        :param hyper: Hyperparameter to set the value of.
+        Setter. Sets the value of a provided hypeparameter. Used for fine-tuning.
+        :param hyper: Hyperparameter whose value needs to be set
         :param value: New value
         """
 
